8a53f62b867182e05e2843073f8841c4e73c9516
chuk-yong <cpyong@cpyongs-MacBook-Pro.local>
2016-07-02 19:53:50 +0800

    Added Comments

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
+# Set working directory and read in the 2 .rds files
 setwd(paste(getwd(),"/NEI_data",sep=""))
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+# Get the total of all pm2.5 emission by year
 TotalByYear <- aggregate(Emissions ~ year, NEI, sum)
 
+# Start plot and save to plot1.png
 png('plot1.png')
 barplot(
   (height=TotalByYear$Emissions)/10^6,
