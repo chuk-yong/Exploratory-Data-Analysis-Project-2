d6ebd59f65bea63bfca358c5584f653d4b54613d
chuk-yong <cpyong@cpyongs-MacBook-Pro.local>
2016-07-03 23:59:27 +0800

    Added dots to line

--- plot6.R
+++ plot6.R
@@ -13,9 +13,9 @@ TotalVehLA <- aggregate(Emissions ~ year, LAVeh, sum)
 
 #plot
 library(ggplot2)
-png("plot5.png")
-g <- ggplot() + geom_line(data=TotalVehBal, aes(year, Emissions, color = "Baltimore")) + geom_point()
-g <- g+ geom_line(data = TotalVehLA, aes(year, Emissions, color = "Los Angeles"))
+png("plot6.png")
+g <- ggplot()+geom_line(data=TotalVehBal, aes(year, Emissions, color = "Baltimore"))+geom_point(data = TotalVehBal,aes(year, Emissions))
+g <- g+ geom_line(data = TotalVehLA, aes(year, Emissions, color = "Los Angeles")) + geom_point(data = TotalVehLA,aes(year, Emissions))
 g <- g+ scale_colour_manual(name="",values = c("Baltimore"= "green","Los Angeles"= "red"))
 g <- g+ xlab("Year") + ylab("Total PM'[2.5] Emissions") + ggtitle("Baltimore vs Los Angeles Vehicle Emissions")
 print(g)
